9097e3748bdd94597850e39c9330b2b7512393ce
John Simiyu <jonnyeinstein@gmail.com>
2022-02-26 02:59:52 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Main {
+
+    //This parser is specifically to data similar in length and type of the provided test data as the json parser
+    //specific to the list of mappings provided
+
+    public static void main(String[] args) {
+
+        //Take list as argument and split into a String array after stripping out the []
+        String s = args[0].substring(1, args[0].length() - 1);
+        String[] s1 = s.split(", ");
+        ArrayList<String> sku = new ArrayList<>();
+
+        //store the sku into an array list to prepare for frequency count
+        for (int i=0; i<s1.length; i++)
+            sku.add(s1[i]);
+
+        //take the mapping of products which is a json and write a small stripping parser after removing []
+        String p = args[1].substring(1, args[1].length() - 2);
+        String p1 = p.replace(": {", "=");
+        String p2 = p1.replace("},", "+");
+
+        //create a hashmap to convert the list of json to a hashmap
+        HashMap<String, String> items = new HashMap<String, String>();
+
+        //Split the json into its components i.e key values
+        String[] pitems = p2.split("\\+");
+        //Store key as hashmap key and value as hashmap value
+        for (int i=0; i<pitems.length; i++)
+            items.put(pitems[i].split("\\=")[0], pitems[i].split("\\=")[1]);
+
+        //Create a hashset to store distinct values of our sku list
+        Set<String> distinct = new HashSet<>(sku);
+        for (String i: distinct) {
+            for (Map.Entry<String, String> set : items.entrySet()) {
+                if(i.equals(set.getKey()))
+                    //update the hashmap key values with frequency count of product skus
+                    items.put(set.getKey(), set.getValue() + ",quantity:" +Collections.frequency(sku, i));
+            }
+        }
+
+        //Stringbuilder to assemble our hashmap back to list of json
+        StringBuilder result = new StringBuilder();
+
+//        for (Map.Entry<String, String> set : items.entrySet()) {
+//            result.append(set.getValue());
+//        }
+
+        //hashmap values joined together to create the list of json skeleton
+        items.forEach((key, value) -> result.append(value + "},{\""));
+
+        //parser to build our list of json output
+        //
+        String r = result.toString();
+        String r1 = r.replace(",", ",\"");
+        String r2 = r1.replace(": ", "\": \"");
+//        String r8 = r2.replace(":", "\": \"");
+        String r5 = r2.replace(",\"", "\",\"");
+        String r3 = r5.replace(":2\"", "\":2");
+        String r4 = r3.replace(":1\"", "\":1");
+        String r6 = r4.replace("\"}\",\"+", "},");
+        String r7 = "[{\"" + r6.substring(0, r6.length() - 5) + "]";
+
+
+        //print output to console.
+        System.out.println("Purchased items: " + r7.replace("\",\"", ","));
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Tractive JAVA Test
+
+Backend Developer Task
+
+SKU, Version, Quantity - Coding Task
+
+
+Write a method that takes two inputs: a list of purchased product codes and a map of mappings for these codes. The method should return an aggregated list of purchased products and quantity based on the list of purchased products codes.
+
+<strong>Inputs</strong>
+
+List of products: ["CVCD", "SDFD", "DDDF", "SDFD"]
+
+Mappings: {"CVCD": {"version": 1,"edition": "X"},"SDFD": {"version": 2,"edition": "Z"},"DDDF": {"version": 1}}
+
+<strong>Expected Output</strong>
+
+Purchased items:
+[{"version":1,"edition":"X","quantity":1},{"version":1,"quantity":1},{"version":2,"edition":"Z","qu antity":2}]
+
+
+##Running the app
+
+```zsh
+➜  tractive java Main.java "["CVCD", "SDFD", "DDDF", "SDFD"]" "{"CVCD": {"version": 1,"edition": "X"},"SDFD": {"version": 2,"edition": "Z"},"DDDF": {"version": 1}}"
+[{"version": "2,edition": "Z,quantity:2},{"version": "1,edition": "X,quantity:1},{"version": "1,quantity:1}]
+```
+
+## JUnit Test
+
+Todo
